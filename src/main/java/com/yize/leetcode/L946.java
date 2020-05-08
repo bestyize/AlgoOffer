1797c414f7b6596d5861a060b8a51f92f04edec3
yize <3011429848@qq.com>
2020-05-08 10:41:39 +0800

    leetcode946

---
+++ src/main/java/com/yize/leetcode/L946.java
@@ -0,0 +1,55 @@
+package com.yize.leetcode;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+/**
+ * 946. 验证栈序列
+ * 给定 pushed 和 popped 两个序列，每个序列中的 值都不重复，只有当它们可能是在最初空栈上进行的推入 push 和弹出 pop 操作序列的结果时，返回 true；否则，返回 false 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+ * 输出：true
+ * 解释：我们可以按以下顺序执行：
+ * push(1), push(2), push(3), push(4), pop() -> 4,
+ * push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+ * 示例 2：
+ *
+ * 输入：pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+ * 输出：false
+ * 解释：1 不能在 2 之前弹出。
+ *
+ *
+ * 提示：
+ *
+ * 0 <= pushed.length == popped.length <= 1000
+ * 0 <= pushed[i], popped[i] < 1000
+ * pushed 是 popped 的排列。
+ */
+public class L946 {
+    @Test
+    public void test(){
+        int[] pushed={1,0};
+        //int[] popped={4,5,3,2,1};
+        int[] popped={1,0};
+        System.out.println(validateStackSequences(pushed,popped));
+    }
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack=new Stack<>();
+        int ptrPop=0;
+        for (int in:pushed){
+            stack.push(in);
+            while (!stack.isEmpty()&&ptrPop<popped.length&&stack.peek()==popped[ptrPop]){
+                stack.pop();
+                ptrPop++;
+            }
+        }
+        return ptrPop==popped.length;
+    }
+
+}
