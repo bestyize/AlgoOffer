61e68221ca0b6bc6f3bc79b1ca6b0b1770b4e177
yize <3011429848@qq.com>
2021-03-04 09:30:32 +0800

    feat:leetcode

--- src/main/java/thewind/bdtest/T17RemoveListElement.java
+++ src/main/java/thewind/bdtest/T17RemoveListElement.java
@@ -1,11 +1,45 @@
 package thewind.bdtest;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
+import java.util.concurrent.CopyOnWriteArrayList;
+//https://blog.csdn.net/PanPa_WoNiu/article/details/81185178
 public class T17RemoveListElement {
-    public static void main(String[] args) {
+    @Test
+    public void test0(){
+        List<String> list=new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        for (String i:list){
+            list.remove(i);
+        }
+        for (String i:list){
+            System.out.print(i+" ");
+        }
+    }
+
+    @Test
+    public void test1(){
+        List<String> list=new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        for (int i=0;i<list.size();i++){
+            list.remove(i);
+        }
+        for (String i:list){
+            System.out.print(i+" ");
+        }
+    }
+
+    @Test
+    public void test2() {
         List<Integer> list=new ArrayList<>();
         list.add(1);
         list.add(2);
@@ -14,12 +48,31 @@ public class T17RemoveListElement {
 
         Iterator<Integer> iterator=list.listIterator();
         while (iterator.hasNext()){
-            if(iterator.next().equals(123)){
-                iterator.remove();
-            }
+            iterator.next();
+            iterator.remove();
+//            if(iterator.next().equals(2)){
+//                iterator.remove();
+//            }
         }
         for (Integer i:list){
             System.out.println(i);
+        }
+    }
+    @Test
+    public void test3(){
+        List<Integer> list=new CopyOnWriteArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        for (Integer i:list){
+            list.remove(i);
+//            if(i==2){
+//                list.remove(i);
+//            }
+        }
+        for (Integer i:list){
+            System.out.print(i+" ");
         }
     }
 }
--- src/main/java/thewind/bdtest/T25RightBiggerThan.java
+++ src/main/java/thewind/bdtest/T25RightBiggerThan.java
@@ -1,16 +1,16 @@
 package thewind.bdtest;
 
 import java.util.Stack;
-
+// 1 3 2 4 5 1 2
 public class T25RightBiggerThan {
     public int[] rightBiggerThan(int[] nums){
         int[] res=new int[nums.length];
         Stack<Integer> stack=new Stack<>();//存储索引
-        stack.push(0);
+        stack.push(0);//没找到右边第一大元素的索引先入栈，本质上是维持了一个左值单调栈，单调栈的上限是未来右边第一大值
         int curr=1;
         while (curr<nums.length){
             if(!stack.isEmpty()&&nums[stack.peek()]<nums[curr]){//当前值小于stack峰顶数
-                res[stack.pop()]=nums[curr];
+                res[stack.pop()]=nums[curr];//找到了后出栈
             }else {
                 stack.push(curr++);
             }
